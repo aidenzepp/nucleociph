36a7ae98f2a201bd58e56c22d087c82482e4b7b6
Aiden Zepp <aiden.zepp@protonmail.com>
2023-07-28 17:46:31 -0400

    Rough draft.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// private imports...
+mod cipher;
+mod nucleo;
+mod phrase;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// public re-exports...
