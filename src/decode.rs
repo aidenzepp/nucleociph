7499a1a1319bc7bae9bd1159f4dffaf9421a7820
Aiden Zepp <aiden.zepp@protonmail.com>
2023-08-01 13:45:40 -0400

    Testing a simpler solution.

---
+++ src/decode.rs
@@ -0,0 +1,6 @@
+//! Holds the logic for decoding a string of text.
+
+/// Returns the encoded cipher of ATGC characters provided into a phrase.
+pub fn decode(cipher: String) -> String {
+    todo!()
+}
---
+++ src/encode.rs
@@ -0,0 +1,7 @@
+//! Holds the logic for encoding a string of text.
+
+
+/// Returns the phrase provided into an encoded cipher of ATGC characters.
+pub fn encode(phrase: String) -> String {
+    todo!()
+}
