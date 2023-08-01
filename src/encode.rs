2ec89abedd57bcfa5f7133abaad4dbfbf0a312c3
Aiden Zepp <aiden.zepp@protonmail.com>
2023-08-01 15:20:42 -0400

    Created and documented encoding logic.

--- src/encode.rs
+++ src/encode.rs
@@ -1,6 +1,5 @@
 //! Holds the logic for encoding a string of text.
 
-
 // Returns the individual bits of a `char` as a `String`.
 fn bits(character: char) -> String {
     format!("{:08b}", character as u8)
@@ -29,15 +28,31 @@ fn process(character: char) -> impl Iterator<Item = char> {
     (0..4).filter_map(move |i| code(pair(&bits, i)))
 }
 
-/// Returns the phrase provided into an encoded cipher of ATGC characters.
+/// Encodes a given phrase into a cipher of ATCG characters.
+///
+/// Each character in the phrase is treated as a sequence of 8 bits, 
+/// which are then grouped into pairs and encoded into 'A', 'T', 'C', 'G' characters. 
+/// 'A' represents the bit pair "00", 'T' represents "01", 'C' represents "10", 
+/// and 'G' represents "11".
+///
+/// # Examples
+///
+/// ```
+/// use nucleociph::encode;
+///
+/// let phrase: String = "Hello!".to_string();
+/// let cipher: String = "TACATCTTTCGATCGATCGGACAT".to_string();
+///
+/// assert_eq!(encode(phrase), cipher);
+/// ```
+///
+/// # Arguments
+///
+/// * `phrase` - A string that is to be encoded into an ATCG cipher.
+///
+/// # Returns
+///
+/// Returns the encoded string, or an empty string if the phrase cannot be encoded.
 pub fn encode(phrase: String) -> String {
     phrase.chars().flat_map(process).collect()
-}
-
-#[test]
-fn works() {
-    let phrase: String = "Hello!".to_string();
-    let cipher: String = "TACATCTTTCGATCGATCGGACAT".to_string();
-    
-    assert_eq!(encode(phrase), cipher);
 }
